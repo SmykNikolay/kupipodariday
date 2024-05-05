c8f3f0ecce3afc2f82921ca3d03a57bda7c22b5f
SmykNikolay <nikolaysmyk613@gmail.com>
2024-05-05 23:48:28 +0300

    clear

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import configuration from './configuration';
-import { UsersModule } from './users/users.module';
-import { WishesModule } from './wishes/wishes.module';
-import { WishlistsModule } from './wishlists/wishlists.module';
-import { OffersModule } from './offers/offers.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -26,11 +21,6 @@ import { AuthModule } from './auth/auth.module';
           synchronize: true,
         }) as TypeOrmModuleOptions,
     }),
-    UsersModule,
-    WishesModule,
-    WishlistsModule,
-    OffersModule,
-    AuthModule,
   ],
 })
 export class AppModule {}
--- src/wishes/wishes.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { WishesService } from './wishes.service';
-
-@Controller('wishes')
-export class WishesController {
-  constructor(private readonly wishesService: WishesService) { }
-}
